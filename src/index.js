e1c33e1165f5fe8e879b546d3b2f24fa4e8ab622
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2025-05-31 20:56:23 +0300

    deploy

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { info, success, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+
+success({
+    title: 'Правильно',
+    delete: 50
+});
